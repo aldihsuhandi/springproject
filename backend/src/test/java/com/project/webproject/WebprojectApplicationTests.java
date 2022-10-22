6900b4158d431fdb00565c33b22cc3cdb3d25587
aldih suhandi <aldihsuhandi697@gmail.com>
2022-10-22 21:43:41 +0700

    initial backend

---
+++ backend/src/test/java/com/project/webproject/WebprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.webproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
